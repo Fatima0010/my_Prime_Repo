fe24f13932f9b735de950db0d1c72d2cb367a6a1
Fatima0010 <bsef23a033@pucit.edu.pk>
2024-10-15 02:40:09 +0500

    Add files via upload

---
+++ Prime_Maze_Generator.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// Function to check if a number is prime
+bool isPrime(int num) 
+{
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; ++i) 
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+// Function to generate a prime number maze
+void generateMaze(int size) 
+{
+    vector<vector<char>> maze(size, vector<char>(size, ' '));
+
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j) 
+        {
+            int num = rand() % 100;  // Random number between 0 and 99
+            if (isPrime(num)) 
+            {
+                maze[i][j] = 'P';  // Mark prime numbers with 'P'
+            }
+            else
+            {
+                maze[i][j] = '.';  // Mark non-prime numbers with '.'
+            }
+        }
+    }
+
+    // Print the maze
+    for (const auto& row : maze)
+    {
+        for (const auto& cell : row)
+        {
+            cout << cell << ' ';
+        }
+        cout << endl;
+    }
+}
+
+int main() 
+{
+    srand(static_cast<unsigned int>(time(0)));  // Seed for random number generation
+    int size;
+
+    cout << "Enter the size of the maze: ";
+    cin >> size;
+
+    generateMaze(size);
+    return 0;
+}
